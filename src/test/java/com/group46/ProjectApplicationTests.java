1c4089398865bae2f7fd0f7a2b97e96e494cafbd
Terran <terranlansakarat20@gmail.com>
2026-04-22 15:46:55 +0530

    first commit

---
+++ src/test/java/com/group46/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group46;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
